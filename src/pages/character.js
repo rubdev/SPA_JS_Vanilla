3aeb4f1ee85c8ed4c411edd7cfce27cb1c37e464
Rubén Segura <rsegrom@gmail.com>
2021-01-21 13:54:51 +0100

    Obtner datos api para el personaje

--- src/pages/character.js
+++ src/pages/character.js
@@ -1,17 +1,25 @@
-const Character = () => {
+import getData from '../utils/getData';
+import getHash from '../utils/getHash';
+
+const Character = async () => {
+    // obtenemos el hash del personaje
+    const id = getHash();
+    // obtenemos los datos del personaje utilizando su id hasheado
+    const character = await getData(id);
+    console.log(character);
     const view = `
         <div class="characters-inner">
             <article class="character-card">     
-                <img src="image" alt="name">
-                <h2>Name</h2>
+                <img src="${character.image}" alt="${character.name}">
+                <h2>${character.name}</h2>
             </article>
             <article class="character-card">
-                <h3>Episodios:</h3>
-                <h3>Estado:</h3>
-                <h3>Especie:</h3>
-                <h3>Género:</h3>
-                <h3>Origen:</h3>
-                <h3>Última localización:</h3>
+                <h3>Episodios: <span>${character.episode.length}</span></h3>
+                <h3>Estado: <span>${character.status}</span></h3>
+                <h3>Especie: <span>${character.species}</span></h3>
+                <h3>Género: <span>${character.gender}</span></h3>
+                <h3>Origen: <span>${character.origin.name}</span></h3>
+                <h3>Última localización: <span>${character.location.name}</span></h3>
             </article>
         </div>
     `;
